c97feda5b5b874f747a33157a50d668853108356
stephengeller <stephen.geller@hotmail.com>
2017-11-11 19:44:56 +0000

    more stripe setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,25 +5,24 @@ import { connect } from 'react-redux';
 class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
-			case null:
-				return;
-			case false:
-				return (
-					<li>
-						<a href="/auth/google">Login with Google</a>
-					</li>
-				);
-			default:
-				return (
-					<li>
-						<a href="/api/logout">Logout</a>
-					</li>
-				);
+		case null:
+			return;
+		case false:
+			return (
+				<li>
+					<a href="/auth/google">Login with Google</a>
+				</li>
+			);
+		default:
+			return (
+				<li>
+					<a href="/api/logout">Logout</a>
+				</li>
+			);
 		}
 	}
 
 	render() {
-		console.log(this.props);
 		return (
 			<nav>
 				<div className="nav-wrapper">
